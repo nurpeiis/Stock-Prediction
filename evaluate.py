d66e0c91cde8516965d230380165ed3dfdfcc2a7
nurpeiis <nb2577@nyu.edu>
2020-12-07 22:37:32 +0600

    Implement expected_variance_score and compute maximum profit using peak valley approach

--- evaluate.py
+++ evaluate.py
@@ -1,5 +1,6 @@
 from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import mean_squared_error
+from sklearn.metrics import explained_variance_score
 from math import sqrt
 
 def calculate_error(real, predicted):
@@ -14,3 +15,15 @@ def calculate_error(real, predicted):
     rmse = sqrt(mse) #root_mean_square
     
     return {'MFE': mfe, 'MAE': mae,  'MSE': mse, 'RMSE': rmse}
+
+def explained_variance(real, predicted):
+    exp_var = explained_variance_score(real, predicted)
+    return exp_var
+
+def compute_profit(real, predicted):
+    profit = 0.0
+    for i in range(1, len(predicted)):
+        if predicted[i] > predicted[i-1]:
+            profit += real[i] - real[i-1]
+    
+    return profit
